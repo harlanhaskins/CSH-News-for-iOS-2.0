c8eb8614769017f96d74fed8bab3a39317079491
Harlan Haskins <harlan@harlanhaskins.com>
2014-12-16 20:56:26 -0500

    Initial commit

---
+++ CSH News/UIViewController+Convenience.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Convenience.swift
+//  CSH News
+//
+//  Created by Harlan Haskins on 12/16/14.
+//  Copyright (c) 2014 csh. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+}
